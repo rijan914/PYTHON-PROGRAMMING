a595a5b8bfaad746d56f249b8ae67d2ec7d2ba9f
rijan914 <alpharijan1@gmail.com>
2024-04-28 01:19:26 +0545

    added day 5

---
+++ day5 Comments-and-Print/d5.py
@@ -0,0 +1,11 @@
+''' /n is used for new line '''
+''' # and ''' ''' is used to comment the code''' ''' ''' 
+# author : Rijan 
+# course : Code with Harry 
+print("hey i am a creating new line  \n like this ")
+print("using forward slash \"good broo\" to use double quotes")
+print("hey",5 ,6, )
+#use sep="~" to separate the values
+print("name","rijan" ,"age", 20, sep="~")
+#end is used to end the line with the given value
+print("50 rupaiya ", end="matra \n")
