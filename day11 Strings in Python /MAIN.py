ee834624da374c466fb89331afe5b348dcbacdac
rijan914 <alpharijan1@gmail.com>
2024-04-28 15:21:21 +0545

    added day 11

---
+++ day11 Strings in Python /MAIN.py
@@ -0,0 +1,14 @@
+#strings in python
+name = input("Enter your name\n")
+class_name = "twelve"
+anothername= 'philips'
+print("Hello, Konichiwa",name)
+#EOL: end of line error
+apple ='''its me ,Tim COOK'''
+print(apple)
+#accessing characters of a string : like array of character
+print(apple[0])
+print(apple[2])
+for character in apple:
+    print(character)
+Strings Slicing and Operations on Strings
