11ffb2a9234c22c895fa09379c22464cf1b88130
rijan914 <alpharijan1@gmail.com>
2024-04-30 01:35:12 +0545

    added day 18 and 19

---
+++ 18-Day18-While-Loops/main.py
@@ -0,0 +1,36 @@
+for i in range(3):
+    print(i)
+
+# i=0
+# while(i<3):
+#     print(i)
+#     i=i+1
+
+
+smoke=str(input("Do you smoke?"))
+if(smoke=='yes'):
+    i=0
+else:i=100
+
+while(i<100):
+    print("What's that brother Stop smoking")
+    i=i+1
+smoke=str(input("WILL you smoke?"))  
+if(smoke=='yes'):
+    print("Jati bhaneni kei kam lagdaina raixa ellai taaaa!")
+else:
+    print("good boyy!! ")
+    
+    #emulate do while loop in python
+    i = 0
+    while True:
+        print(i)
+        i += 1
+        if i >= 3:
+            break
+    i=int(input("enter number"))
+    print(i)
+    while(i<=23):
+        i=int(input("enter number"))
+        print(i)
+        
