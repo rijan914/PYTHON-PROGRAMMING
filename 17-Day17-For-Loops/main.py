1945068c8ca127c41e2390c45eacccf65493b5a3
rijan914 <alpharijan1@gmail.com>
2024-04-30 01:09:45 +0545

    added day 17

---
+++ 17-Day17-For-Loops/main.py
@@ -0,0 +1,10 @@
+name='Rijan'
+for i in name:
+    print(i,end=' ')
+
+colors = ['red', 'green', 'blue', 'yellow']
+for x in colors:
+        print(x)
+    
+for k in range(-1,10,3) :#means 0 to 4 
+    print(k+1)
