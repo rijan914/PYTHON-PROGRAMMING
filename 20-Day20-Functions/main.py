1764d38d1d3b2fabd72ef434e026fa52f4b6c349
rijan914 <alpharijan1@gmail.com>
2024-04-30 09:02:46 +0545

    added day20

---
+++ 20-Day20-Functions/main.py
@@ -0,0 +1,22 @@
+#functions
+def calculategmean(a,b):
+    mean=a*b/(a+b)
+    print(mean)
+def calculategreat(a,b):
+    if(a>b):
+        print("first number is greater")
+    else:
+        print("second number is greater")
+        
+def islower(a,b):
+    pass
+
+a=8 
+b=9
+calculategreat(a,b)
+calculategmean(a,b)
+d=3
+c=2
+calculategmean(c,d)
+calculategreat(c,d)
+
