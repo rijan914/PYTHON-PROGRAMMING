035064cb47b4767b084b1b70869ccf70600e1a53
rijan914 <alpharijan1@gmail.com>
2024-04-30 08:48:12 +0545

    added day19

---
+++ 19-Day-19-break-and-continue/main.py
@@ -0,0 +1,10 @@
+#break and continue 
+print("Multiplication table of 5")
+for i in range(12):
+    print("5*",i,"=",5*i)
+    if(i==10):
+        continue
+print("over")
+#continue skips the iteration 
+#break skips the loop 
+
