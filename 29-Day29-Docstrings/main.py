9efce87a3232659be856a283398d191f07dec4bc
rijan914 <alpharijan1@gmail.com>
2024-05-01 14:58:31 +0545

    added day29

---
+++ 29-Day29-Docstrings/main.py
@@ -0,0 +1,14 @@
+#docstring
+# This is the main file for the project. It will be used to run the project.
+def square(n):
+    '''takes in a  number n , returns the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+#pep8
+#pep8 is a python enhancement proposal that is used to format python code in a way that is easy to read and understand
+#it is a set of rules that is used to format python code
+
+#zen of python
+#zen of python is a collection of 19 software principles that influences the design of python programming language
