d37141204b71a51eec0324722acabc5995cfe5d1
rijan914 <alpharijan1@gmail.com>
2024-05-01 18:57:19 +0545

    added day 35

---
+++ 35-Day-35-For-loop-with-else/main.py
@@ -0,0 +1,17 @@
+#for loop with else statements
+# for i in range(5):
+#     print(i)
+# else:
+#     print("sorry no i ")
+    
+# for i in []:
+#     print(i)
+# else:
+#     print("sorry no i ")
+    
+for i in range(6):
+    print(i)
+    if i==4:
+        break 
+else: 
+    print("sorry no i")
