700245b6560ad31cb9fd4cce4abca9c6e95be727
rijan914 <alpharijan1@gmail.com>
2024-05-20 11:06:31 +0545

    added day 50

---
+++ 50-Day50-read-readlines-and-other-methods/main.py
@@ -0,0 +1,205 @@
+
+
+BCT Engineering Notes
+Giving Engineering Students Their Life Back
+Digital Signal Processing 
+- March 14, 2020
+
+
+
+Total Days If Complete Newbie and Never Started, Don't Know Anything :-  Very Hard Days  to pass [7 Days Before Exam + 4 Days Mid Semester Study]
+Goal - Score Above 45 /80 in the finals  [Aim high if difficult question comes you can pass]
+
+The subject is 95% Numerical .This subject be finished for about 45-50 marks mid semester. There are limited theory [5 % ] don't expect to pass by studying just theory. It's impossible.
+
+This exam is the first exam of the semester so start a week ahead in advance try to finish 2 days before exam. You will certainly miss the mark and finish in the evening of the day before. 
+
+It will take you 1 and half day after sitting down to study to just start studying.
+
+Numerical & Theory
+
+Finish The Tutorial Question Listed below and  old question collection numericals.  
+
+As Numerical are connected with theory so see the theory carefully and practice the numerical carefully. Finish Numerical First.
+
+Watch The YouTube Videos First Then Only go to the notes as it will help you to get a overview of how to solve.
+
+YouTube [Extremely Recommended]
+
+1. Chapter 1 - Discrete Time Signals And Systems
+2. Chapter 2 - Z-transform
+3. Chapter 3 - Analysis Of LTI system In Frequency Domain
+4. Chapter 4 - Discrete Filter Structures
+5. Chapter 5 - Kaiser Window
+6. Chapter 6 - Butterworth Filter
+7. Chapter 7 - DITFFT, Butterfly DIagram & Circular Convolution [Completely6 Covered]
+
+Notes [Extremely Recommended]
+
+Syllabus
+List Of Important Topics [Extremely Recommended]
+1. Chapter 1 - Discrete Time Signals And Systems [9 marks]
+2. Chapter 2 - Z-transform [6 marks]
+3. Chapter 3 - Analysis Of LTI system In Frequency Domain [10 Marks]
+4. Chapter 4 - Discrete Filter Structures [10 Marks]
+5. Chapter 5 - FIR filter Design [15 Marks]
+6. Chapter 6 - IIR filter Design [15 Marks]
+7. Chapter 7 - Discrete Fourier Transform [15 Marks]
+
+All Chapter Notes Summary [Recommended]
+IRR Notes [Recommended]
+FIR Notes [Recommended]
+Z Transform Notes [Recommended]
+
+All Chapter Complete Notes 1
+All Chapter Complete Notes 2
+Old Question[Extremely Recommended]
+
+Old Question Collection 
+List Of Important Topics [Extremely Recommend]
+List Of Important Topics2
+
+Tutorial Question Collection 1
+Tutorial Question Collection 2
+IRR Numerical 
+
+For Someone who is interested in College Video Tutorial 
+
+DSAP Video Tutorial
+
+Further Reading [If Super Interested]
+
+Suresh Sharma Digital Signal Processing
+Oppenheim Solution Manual 
+Principle Algorithm And Application Solution Manual 
+
+
+
+Unit
+Hours
+Mark Distribution*
+1.Discrete signals                                              
+                      8                     
+9
+2..Z transform                                                    
+4
+6
+     3.Analysis of LTI system in frequency domain    
+6
+10
+  4.Discrete filter structures                                    
+8
+10
+5.FIR filter design                                                 
+6
+15
+6.IIR Filter Design                                                
+6
+15
+   7.Discrete Fourier transform                                 
+7
+15
+Total                
+45
+80
+
+7th Sem
+
+Comments
+
+Anonymous27 May
+plz add old question solution asap
+
+REPLY
+
+Engineering Student :)27 May
+Don't have it. Try in pulchowk
+
+
+Anonymous28 August
+https://www.youtube.com/playlist?list=PLtnAi4Q6UCpq-fxJwf6dIE-5psoJOtq9i
+
+REPLY
+
+Engineering Student :)31 August
+Thank You for the playlist. We have added the Playlist. If you have more such playlist, feel free to leave it in the comment section.
+
+
+Anonymous23 March
+https://github.com/Noconfiesenmiscodigos/IOE-Digital-Signal-Analysis-And-Processing-Lab-Code-Solutions
+
+REPLY
+
+Anonymous24 May
+dai sir le padako wala video ko note xa vane haldinu na
+
+REPLY
+
+Engineering Student :)27 May
+Sir le ta padako video chaina. Maile ta aafai padeko ho.
+
+
+Amit Pant31 July
+A good playlist:
+
+https://www.youtube.com/playlist?list=PLULQVvZuQOEYkrEuu37-5ZDFNCXIAaIb4
+
+REPLY
+
+Anonymous22 August
+Hello vai
+
+
+Anonymous22 August
+Hello mero vai. Ji gara .
+
+
+Anonymous17 August
+Filter Design:
+https://www.youtube.com/playlist?list=PLN7f6qVfiv53vo61Jn52rDlQnmvfBJ3-L
+
+REPLY
+
+Anonymous22 August
+Nice comment
+
+
+Anonymous02 September
+notes are not in a respective links
+
+REPLY
+
+Anonymous08 September
+bro bharkhar ko bharkhar circular convolution video gayab vayo ta
+
+REPLY
+
+Anonymous10 September
+Same college video/lectures as mentioned above, but the following playlist has proper naming of chapters and timestamp in thumbnail and description:
+https://www.youtube.com/playlist?list=PLitcWa0jogCM1Je7djdER97Fg_3Xqn7QN
+
+REPLY
+
+Anonymous10 January
+Thank you for the guidance i got 64 marks in this subject
+
+REPLY
+
+Post a Comment
+Any Advice,Suggestion or Request For New Material. Please Post Here
+
+Popular posts from this blog
+Computer Network
+- December 27, 2020
+ Image
+Total Days If Complete Newbie and Never Started, Don't Know Anything :- 6 Days. [3.5 days before exam month and 2.5 days of exam month]  Goal - Score More than 60/80 Marks in final exam Chapter 4 Numerical + Chapter 8 Numerical [1 Day] [Finish Thoroughly Before Exam Month] Chapter 1,5,8 [1.5 Day]  [Finish Before Exam Month] [Finish Them and remember the Header(Ch. 5) And Algorithm(Ch. 8) ] Chapter 2,3,4,6,7 [Just Watch The YouTube Videos Thoroughly No Worries] [1 Days] At Exam Month Chapter 1,4 [1st Day Evening + 2nd Day Morning]  Chapter 2,3,5 [2nd Day]  [Easy + Easy + Hard] Chapter 6,7,8 [3rd Day]  [Easy + Easy + Hard] To guarantee to pass, starting at the exam month is not a sure way like other subjects.   The chapters with most marks are hard and you  always run into the probability that from the chapters you have studied  hard questions or unexpected question come.  Concerning the theory the notes are enough and also for the Numerical of chapter 8 can be easily understood  
+READ MORE
+Engineering Economics
+- May 09, 2019
+ Image
+Syllabus  Important Topics To Focus On Important Theory only 1. Chapter 1 - Introduction to Engineering Economics 2. Chapter 2 - Cost Concepts and Behavior 3. Chapter 3 - Understanding Financial Statements                   3.1 -  Chapter 3 Remaining 4. Chapter 4 - Comparative analysis of alternatives 5. Chapter 5 - Replacement Analysis 6. Chapter 6 - Risk Analysis 7. Chapter 7 - Income Tax & Discounted Cash-flow models 8. Chapter 8 - Project Risk Analysis Good Handwriting All Chapter Notes  [Recommended] GL Book House In Writing Class Note Of 2075 BCT A All Chapter Notes Tutorial Old Question Collection Important Questions Tutorial 1 : Chapter 2 Tutorial 2 : Chapter 3 Tutorial 3 : Chapter 4 Tutorial 4 : Chapter 7 Tutorial 5 : Chapter 8 YouTube All Chapter Formulas Lectures 1.   Lecture 1 - Introduction Into Economics 2.   Lecture 2 - Financial Ratio 3.   Lecture 3 - Time Value Of Money 4.   Lecture 4 - Understanding Money and
+READ MORE
+ Powered by Blogger
+Theme images by Deejpilot
+BCT Engineering Notes
+
